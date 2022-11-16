7f1cb07b3e123edb4054e7ac39e91eacadfaf553
Bianca Macedo Hoffer Madruga <bianca.mhm1@gmail.com>
2022-11-16 20:48:27 -0300

    teste

--- src/Pages/Home/styles.ts
+++ src/Pages/Home/styles.ts
@@ -1,8 +1,7 @@
 import styled from 'styled-components';
-import backgroundImage from '../../assetsBackground.png';
+//import backgroundImage from '../../assetsBackground.png';
 
 export const HomeContainer = styled.main`
-    background-image: ${() => `url(${backgroundImage})`};
     width: 100%;
     height: 34rem;
 `;
